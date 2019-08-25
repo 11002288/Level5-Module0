6fba3a3c1a07299b1c86d7477d3efa46a751487e
League <league@iMac.attlocal.net>
2019-08-25 11:30:12 -0700

    done
    Start 03

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,24 @@
 package _01_File_Recorder;
 
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
+	public static void main(String[] args) {
+		String a = JOptionPane.showInputDialog("Write a Message");
+		try {
+			FileWriter fw = new FileWriter("src/_01_File_Recorder/tester.txt");
+			
+			fw.write(a);
+			fw.close();
+		}
+		catch(IOException e){
+			e.printStackTrace();
+			
+		}
+	}
 }
--- src/_02_File_Encrypt_Decrypt/FileEncryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileEncryptor.java
@@ -1,7 +1,30 @@
 package _02_File_Encrypt_Decrypt;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileEncryptor {
 	// Create a program that takes a message from the user.
 	// Use the methods in the String and Character classes to save
 	// an encrypted form of the message to a file
-}
+	public static void main(String[] args) {
+		String a = JOptionPane.showInputDialog("Write a Message");
+		if(a.contains("a")) {
+			
+				a = a.replaceAll("a", "@");
+			}
+		try
+
+	{
+		FileWriter fw = new FileWriter("src/_02_File_Encrypt_Decrypt/messager");
+
+		fw.write(a);
+		fw.close();
+	}catch(
+	IOException e)
+	{
+		e.printStackTrace();
+	}
+}}
