a58de59cb722ed5b546d5172acce343bdc027c83
League <league@iMac.attlocal.net>
2019-09-01 11:29:25 -0700

    asdf
    asdf

---
+++ src/_03_To_Do_List/Driver.java
@@ -0,0 +1,9 @@
+package _03_To_Do_List;
+
+public class Driver {
+public static void main(String[] args) {
+	new ToDoList();
+		
+
+	}
+}
--- src/_03_To_Do_List/ToDoList.java
+++ src/_03_To_Do_List/ToDoList.java
@@ -1,24 +1,60 @@
 package _03_To_Do_List;
 
-public class ToDoList {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ToDoList implements ActionListener {
 	/*
-	 * Create a program with five buttons, add task, view tasks, remove task, save list, and load list. 
+	 * Create a program with five buttons, add task, view tasks, remove task, save
+	 * list, and load list.
 	 * 
-	 * When add task is clicked:
-	 * 		ask the user for a  task and save it to an array list
+	 * When add task is clicked: ask the user for a task and save it to an array
+	 * list
 	 * 
-	 * When the view tasks button is clicked:
-	 *		show all the tasks in the list
+	 * When the view tasks button is clicked: show all the tasks in the list
 	 * 
-	 * When the remove task button is clicked:
-	 * 		prompt the user for which task to remove and take it off of the list.
+	 * When the remove task button is clicked: prompt the user for which task to
+	 * remove and take it off of the list.
 	 * 
-	 * When the save list button is clicked:
-	 * 		Save the list to a file
+	 * When the save list button is clicked: Save the list to a file
 	 * 
-	 * When the load list button is clicked:
-	 * 		Prompt the user for the location of the file and load the list from that file
+	 * When the load list button is clicked: Prompt the user for the location of the
+	 * file and load the list from that file
 	 * 
-	 * When the program starts, it should automatically load the last saved file into the list.
+	 * When the program starts, it should automatically load the last saved file
+	 * into the list.
 	 */
+	public  ToDoList(){
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JButton add = new JButton("Add Task");
+		JButton view = new JButton("View Task");
+		JButton remove = new JButton("Remove Task");
+		JButton save = new JButton("Save List");
+		JButton load = new JButton("Load List");
+		frame.add(panel);
+		panel.add(add);
+		panel.add(view);
+		panel.add(remove);
+		panel.add(save);
+		panel.add(load);
+		frame.setSize(250, 150);
+		frame.setVisible(true);
+		
+	}
+	
+	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
